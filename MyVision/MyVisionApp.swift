c160e4f0d24318d064afa73936198474cf0856c5
Alex Hudson <AHudson@tql.com>
2023-06-21 18:55:34 -0400

    Initial Commit

---
+++ MyVision/MyVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVisionApp.swift
+//  MyVision
+//
+//  Created by Alex Hudson on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MyVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
